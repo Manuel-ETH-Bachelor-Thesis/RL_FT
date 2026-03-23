f18a18f9827868d7e0f214635835db7f06d9f581
Manuel Leutwyler <manuel.leutwyler@hispeed.ch>
2026-03-23 15:13:29 +0100

    Train stable_baselines3 SAC model for demonstration purposes.

---
+++ scripts/SAC_inference.py
@@ -0,0 +1,32 @@
+import os
+import gymnasium as gym
+import envs  # Registers your custom Move-v0
+from stable_baselines3 import SAC
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+xml_path = os.path.join(BASE_DIR, "resources", "robots", "unitree_g1", "scene_with_cam.xml")
+model_path = os.path.join(BASE_DIR, "models", "pre_trained", "sac_move")
+
+env = gym.make('Move-v0', xml_path=xml_path, render_mode="human", camera_name="side_tracker")
+
+model = SAC.load(model_path)
+
+num_test_episodes = 5
+
+for ep in range(num_test_episodes):
+    obs, _ = env.reset()
+    episode_reward = 0.0
+    done = False
+    
+   
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        
+        obs, reward, terminated, truncated, _ = env.step(action)
+        episode_reward += reward
+        
+        done = terminated or truncated
+        
+    print(f"Episode {ep + 1} Complete! Total Reward: {episode_reward:.2f}")
+
+env.close()
---
+++ scripts/train_SAC.py
@@ -0,0 +1,53 @@
+import os
+import gymnasium as gym
+import envs
+import matplotlib.pyplot as plt
+from stable_baselines3 import SAC
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+class MyCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+        self.episode = 0
+        self.rewards = []
+        plt.ion()
+        self.fig, self.ax = plt.subplots()
+
+    def _on_step(self) -> bool:
+        if "episode" in self.locals["infos"][0]:
+            r = self.locals["infos"][0]["episode"]["r"]
+            self.rewards.append(r)
+            print(f"Episode {self.episode} - Reward: {r}")
+            if self.episode % 5 == 0:
+                self.ax.clear()
+                self.ax.plot(self.rewards, label="SAC Training", color='blue')
+                self.ax.set_xlabel("Episode")
+                self.ax.set_ylabel("Total Reward")
+                self.ax.set_title("Stable Baselines3 SAC Progress")
+                self.ax.legend()
+                plt.pause(0.001)
+            self.episode += 1
+        return True
+
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+def make_env():
+    def _init():
+        env = gym.make('Move-v0', xml_path=os.path.join(BASE_DIR, "resources", "robots", "unitree_g1", "scene.xml"))
+        return Monitor(env)
+    return _init
+
+if __name__ == "__main__":
+    num_envs = 6
+    env = SubprocVecEnv([make_env() for _ in range(num_envs)])
+
+    model = SAC("MlpPolicy", env, verbose=0, seed=42, device="cpu")
+    total_episodes = 5_000_000
+    print(f"Starting training on {num_envs} cores")
+    model.learn(total_timesteps=total_episodes, callback=MyCallback())
+    model.save(os.path.join(BASE_DIR, "models", "sac_move"))
+
+    env.close()
